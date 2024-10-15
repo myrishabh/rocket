4d0bd151301a8cd19304af95e175a79730175678
myrishabh <79375142+myrishabh@users.noreply.github.com>
2024-10-15 14:54:19 +0530

    Delete rocket directory

--- rocket/controllers/index.js
+++
@@ -1,3 +0,0 @@
-const UserController = require("./UserController");
-
-module.exports = {UserController}
--- rocket/routes/userRoute.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const appRoute = express.Router();
-const userRoute = require('../controllers/UserController');
-
-appRoute.get('/getUser', userRoute.getUsers);
-appRoute.post('/postData', userRoute.postData);
-
-
-module.exports = appRoute
